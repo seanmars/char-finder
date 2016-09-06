0da6fd8a5ae108c910e606c20b15801a9b3f6063
Sean.Mars <mars@tizsoft.com>
2016-09-06 15:13:02 +0800

    First commit.

---
+++ CharacterFinder/MainForm.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace CharacterFinder
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxDir = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonFind = new System.Windows.Forms.Button();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.buttonFolder = new System.Windows.Forms.Button();
+            this.richTextBoxLog = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxDir
+            // 
+            this.textBoxDir.Location = new System.Drawing.Point(12, 41);
+            this.textBoxDir.Name = "textBoxDir";
+            this.textBoxDir.Size = new System.Drawing.Size(395, 22);
+            this.textBoxDir.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(484, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStripMain";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // buttonFind
+            // 
+            this.buttonFind.Location = new System.Drawing.Point(332, 69);
+            this.buttonFind.Name = "buttonFind";
+            this.buttonFind.Size = new System.Drawing.Size(75, 23);
+            this.buttonFind.TabIndex = 2;
+            this.buttonFind.Text = "Find";
+            this.buttonFind.UseVisualStyleBackColor = true;
+            this.buttonFind.Click += new System.EventHandler(this.buttonFind_Click);
+            // 
+            // buttonFolder
+            // 
+            this.buttonFolder.Location = new System.Drawing.Point(413, 39);
+            this.buttonFolder.Name = "buttonFolder";
+            this.buttonFolder.Size = new System.Drawing.Size(23, 23);
+            this.buttonFolder.TabIndex = 3;
+            this.buttonFolder.Text = "...";
+            this.buttonFolder.UseVisualStyleBackColor = true;
+            this.buttonFolder.Click += new System.EventHandler(this.buttonFolder_Click);
+            // 
+            // richTextBoxLog
+            // 
+            this.richTextBoxLog.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.richTextBoxLog.Location = new System.Drawing.Point(0, 104);
+            this.richTextBoxLog.Name = "richTextBoxLog";
+            this.richTextBoxLog.Size = new System.Drawing.Size(484, 307);
+            this.richTextBoxLog.TabIndex = 4;
+            this.richTextBoxLog.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 411);
+            this.Controls.Add(this.richTextBoxLog);
+            this.Controls.Add(this.buttonFolder);
+            this.Controls.Add(this.buttonFind);
+            this.Controls.Add(this.textBoxDir);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Character Finder";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxDir;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button buttonFind;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Button buttonFolder;
+        private System.Windows.Forms.RichTextBox richTextBoxLog;
+    }
+}
+
---
+++ CharacterFinder/MainForm.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace CharacterFinder
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+        }
+
+        private static bool IsVailed(string txt)
+        {
+            /*
+             * Japan character systen unicode range
+             * U + 4E00–U + 9FBF Kanji
+             * U + 3040–U + 309F Hiragana
+             * U + 30A0–U + 30FF Katakana
+             * return text.Any(c => c >= 0x20000 && c <= 0xFA2D);
+             * */
+
+            return !txt.Any(c =>
+                c >= 0x4E00 && c <= 0x9FBF ||
+                c >= 0x3040 && c <= 0x309F ||
+                c >= 0x30A0 && c <= 0x30FF
+                );
+        }
+
+        private static IList<string> FindNonAlphaNumericLines(IList<string> lines)
+        {
+            var result = new List<string>();
+            if (lines == null || !lines.Any())
+            {
+                return result;
+            }
+
+            var count = 0;
+            foreach (var line in lines)
+            {
+                count++;
+                if (IsVailed(line))
+                {
+                    continue;
+                }
+
+                result.Add($"{count}: {line}");
+            }
+
+            return result;
+        }
+
+        private static string RemoveComment(string input)
+        {
+            const string blockComments = @"/\*(.*?)\*/";
+            const string lineComments = @"//(.*?)\r?\n";
+            const string strings = @"""((\\[^\n]|[^""\n])*)""";
+            const string verbatimStrings = @"@(""[^""]*"")+";
+
+            var noComments = Regex.Replace(input,
+                blockComments + "|" + lineComments + "|" + strings + "|" + verbatimStrings,
+                me =>
+                {
+                    if (me.Value.StartsWith("/*") || me.Value.StartsWith("//"))
+                    {
+                        // return me.Value.StartsWith("//") ? Environment.NewLine : "";
+                        return string.Empty;
+                    }
+
+                    // Keep the literal strings
+                    return me.Value;
+                },
+                RegexOptions.Singleline);
+
+            return noComments;
+        }
+
+        private static IList<string> ReadFile(string path)
+        {
+            var lines = new List<string>();
+            var result = new List<string>();
+            if (!File.Exists(path))
+            {
+                return lines;
+            }
+
+            var src = File.ReadAllText(path);
+            src = RemoveComment(src);
+            var srcAry = src.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+            lines.AddRange(srcAry);
+
+            result.AddRange(FindNonAlphaNumericLines(lines));
+            return result;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonFind_Click(object sender, EventArgs e)
+        {
+            var path = textBoxDir.Text;
+
+            var files = Directory.GetFiles(path, "*.cs", SearchOption.AllDirectories);
+            richTextBoxLog.Clear();
+            var reslut = new List<string>();
+            foreach (var file in files)
+            {
+                var tmp = ReadFile(file);
+                if (!tmp.Any())
+                {
+                    continue;
+                }
+
+                reslut.Add(file + Environment.NewLine);
+                reslut.AddRange(tmp);
+            }
+
+            foreach (var txt in reslut)
+            {
+                richTextBoxLog.AppendText(txt + Environment.NewLine);
+            }
+        }
+
+        private void buttonFolder_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            textBoxDir.Text = folderBrowserDialog.SelectedPath;
+        }
+    }
+}
